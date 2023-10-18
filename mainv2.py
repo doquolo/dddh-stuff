e66fe153b6b377dbf39399d4378f6280d259edd3
doquolo <doquolo123@outlook.com>
2023-10-19 01:39:45 +0700

    di ngu thoi ^^

--- mainv2.py
+++ mainv2.py
@@ -1,16 +1,48 @@
+# GUI
 import PySimpleGUI as sg
 sg.set_options(dpi_awareness=True)
 
+# numpy
 import numpy as np
 
+# matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.lines import Line2D
 
+# mf
 import multiprocessing as mf
 
+# serial
 import serial
 import serial.tools.list_ports
+
+# excel
+from openpyxl.styles import Color, PatternFill, Font, Border
+from openpyxl.styles import colors
+from openpyxl.cell import Cell
+from openpyxl import Workbook
+
+# system
+import os
+import time
+import datetime
+
+# export to excel workbook
+def exportExcel(x, t, path):
+    
+    # create a workbook
+    wb = Workbook()
+    ws = wb.active
+
+    # append data to worksheet
+    # header
+    ws.append(["Thời gian", "Li độ"])
+    # data
+    for i in range(len(x)):
+        ws.append([x[i], t[i]])
+
+    wb.save(path)
 
 # serial
 # trinh chon cong com
@@ -63,11 +95,11 @@ def update(x_out, y_out, currentSerial):
         if (int(ser.in_waiting) > 0):
             sout_raw = ser.readline().decode("utf-8")
             sout = sout_raw.split(";")
-            print(sout)
+            # print(sout)
             try:
                 x_val = int(sout[1])
                 y_val = int(sout[0])
-                print(x_val, y_val)
+                # print(x_val, y_val)
                 x_out.put(x_val)
                 y_out.put(y_val)
             except Exception as e:
@@ -85,8 +117,16 @@ def draw_figure_w_toolbar(canvas, fig):
 
 if __name__ == "__main__":
     # create gui
+    menu = [
+        ['&Tệp', ['&Xuất đồ thị...', '&Thoát']],
+        ['&Đồ thị', ['&Tạo mới']],
+        ['&Trợ giúp', ['&Thông tin']],
+    ]
+
     layout = [
+        [sg.Menu(menu, tearoff=False, key='-menu-')],
         [sg.Canvas(key='fig_cv', size=(500 * 2, 700))],
+        [sg.StatusBar("Chờ kết nối...", key="-status-", expand_x=True, background_color="#fff", text_color="#000", relief=sg.RELIEF_FLAT, pad=(0, 0), size=(10,1))]
     ]
 
     window = sg.Window(f'OscView', layout, background_color='#ffffff')
@@ -124,27 +164,68 @@ if __name__ == "__main__":
     x = []
     y = []
 
+    start_time = 0
+
     while True:
         event, values = window.read(timeout=5)
 
-        if event == sg.WIN_CLOSED:
+        if event == sg.WIN_CLOSED or event == "Thoát":
             break
+
+        if event == "Tạo mới":
+            choice = sg.popup_ok_cancel("Tạo mới đồ thị sẽ xoá hết dữ liệu đồ thị cũ. \nBạn có muốn tiếp tục?", title="Xác nhận!", button_color="#000", background_color="#fff", text_color="#000")
+            if (choice == "OK"): x, y, start_time = [], [], time.time()
+
+        if event == "Xuất đồ thị...":
+            dir = str(os.getcwd())
+            name = datetime.datetime.now()
+            name = name.strftime("%d%m%y_%H%M%S") + ".xlsx"
+            layout = [
+                [
+                    sg.Text("Chọn thư mục: ", background_color='#eeeeee', text_color='#000'), 
+                    sg.Input(key="-IN2-" ,change_submits=True, default_text=dir, background_color='#fff', text_color='#000', border_width=0), 
+                    sg.FolderBrowse(key="-IN-", button_color=('#fff', '#000'))
+                ],
+                [
+                    sg.Button("Chọn", key="Submit", button_color=('#fff', '#000'))
+                ]
+            ]
+            exp_win = sg.Window("Xuất đồ thị", layout, finalize=True, background_color='#eeeeee')
+            while True:
+                event, values = exp_win.read()
+                if event == sg.WIN_CLOSED or event=="Exit":
+                    break
+                elif event == "Submit":
+                    dir = values["-IN2-"]
+                    dir = dir + f"/{name}"
+                    exportExcel(x, y, dir)
+                    sg.Popup(f"Đã xuất {name} tại đường dẫn {dir}.", title="Hoàn tất", background_color='#eeeeee', text_color='#000', button_color=('#fff', '#000'))
+                    break
+            exp_win.close()
+        
+        # update status bar
 
         # read data
         if not currentSerial.empty():
             ser_desc = str(currentSerial.get())
             window.TKroot.title(f"OscView - {ser_desc}")
+            start_time = time.time()
         while not x_out.empty():
+            window["-status-"].update(f"Điểm dữ liệu đã thu thập: x-{len(x)}, y-{len(y)} | Thời gian đã trôi qua: {int(time.time() - start_time)} (s)")
             x.append(x_out.get())
             y.append(y_out.get())
 
         # trim data to only 1000 sample onscreen
         if (len(x) > max_data_points):
-            x = x[-max_data_points:]
-            y = y[-max_data_points:]
+            x_stripped = x[-max_data_points:]
+            y_stripped = y[-max_data_points:]
+            # Update the Line2D object with the new stripped data
+            line.set_data(x_stripped, y_stripped)
+        else:
+            # Update the Line2D object with the new data
+            line.set_data(x, y)
 
-        # Update the Line2D object with the new data
-        line.set_data(x, y)
+        # print(len(x), " ", len(y))
         
         # auto adjust plot view + draw
         ax.autoscale()
